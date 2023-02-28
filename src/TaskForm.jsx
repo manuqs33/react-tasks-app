d22c85a2d07de76663b7145a4344452062316b97
manu <you@example.com>
2023-02-28 11:10:28 -0300

    Adds Bulma and Saas to the packages, reorganizes code

--- src/TaskForm.jsx
+++ src/TaskForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-export default function TaskForm({createTask}) {
+export default function TaskForm({ createTask }) {
 
     const [title, setTitle] = useState("")
     const [description, setDescription] = useState("")
@@ -14,19 +14,24 @@ export default function TaskForm({createTask}) {
     }
 
     return (
+
         <form onSubmit={handleSubmit} method="post">
-            <input
-            type="text"
-            onChange={(e) => setTitle(e.target.value)}
-            placeholder="Write down your task"
-            />
-            <textarea name="description-text" cols="20" rows="5"
-            onChange={(e) => setDescription(e.target.value)}
-            placeholder="Write down your description"
-            />
-            <button>
-                Save
-            </button>
-        </form>
+            <div className="columns">
+                <div className="column">
+                    <input
+                        type="text"
+                        onChange={(e) => setTitle(e.target.value)}
+                        placeholder="Write down your task"
+                    />
+                    <textarea name="description-text" cols="20" rows="5"
+                        onChange={(e) => setDescription(e.target.value)}
+                        placeholder="Write down your description"
+                    />
+                    <button>
+                        Save
+                    </button>
+                </div>
+            </div>
+        </form >
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import 'bulma/css/bulma.min.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
